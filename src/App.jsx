70a49c42404f3c2ab39a2ce9abf868e77566283d
tenthin <tenthinten@gmail.com>
2026-02-04 12:01:31 -0500

    prevent localStorage overwrite on app initialization

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./layout/Dashboard";
 import Home from "./pages/Home";
 import Expense from "./pages/Expense";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
+  const [transactions, setTransactions] = useState(() => {
+    const stored = localStorage.getItem("transactions");
+    return stored ? JSON.parse(stored) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }, [transactions]);
 
   const addTransaction = (newTransaction) => {
     setTransactions((prev) => [...prev, newTransaction]);
@@ -27,7 +34,15 @@ function App() {
             path="/expenses"
             element={<Expense transactions={transactions} />}
           />
-          <Route path="/add-expense" element={<Home transactions={transactions} addTransaction={addTransaction}/>}/>
+          <Route
+            path="/add-expense"
+            element={
+              <Home
+                transactions={transactions}
+                addTransaction={addTransaction}
+              />
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
